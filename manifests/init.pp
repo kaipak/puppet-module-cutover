14d625edb8ee81f29bb991b9d5eee27212f0c704
Chris Matteson <chris.matteson@puppetlabs.com>
2015-09-03 20:15:15 -0700

    added through 3.1.3

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,6 +14,7 @@ class cutover (
     '2.7.19 (Puppet Enterprise 2.6.0)': { $uninstallver='PE/2.6.0' }
     '2.7.19 (Puppet Enterprise 2.7.0)': { $uninstallver='PE/2.7.0' }
     '2.7.19 (Puppet Enterprise 2.7.1)': { $uninstallver='PE/2.7.1' }
+    '2.7.19 (Puppet Enterprise 2.7.2)': { $uninstallver='PE/2.7.2' }
     '2.7.21 (Puppet Enterprise 2.8.0)': { $uninstallver='PE/2.8.0' }
     '2.7.21 (Puppet Enterprise 2.8.1)': { $uninstallver='PE/2.8.1' }
     '2.7.21 (Puppet Enterprise 2.8.2)': { $uninstallver='PE/2.8.2' }
@@ -23,6 +24,14 @@ class cutover (
     '2.7.25 (Puppet Enterprise 2.8.6)': { $uninstallver='PE/2.8.6' }
     '2.7.26 (Puppet Enterprise 2.8.7)': { $uninstallver='PE/2.8.7' }
     '2.7.26 (Puppet Enterprise 2.8.8)': { $uninstallver='PE/2.8.8' }
+    '3.2.2 (Puppet Enterprise 3.0.0)':  { $uninstallver='PE/3.0.0' }
+    '3.2.4 (Puppet Enterprise 3.0.1)':  { $uninstallver='PE/3.0.1' }
+    '3.3.1 (Puppet Enterprise 3.1.0)':  { $uninstallver='PE/3.1.0' }
+    '3.3.3 (Puppet Enterprise 3.1.1)':  { $uninstallver='PE/3.1.1' }
+    '3.3.3 (Puppet Enterprise 3.1.2)':  { $uninstallver='PE/3.1.2' }
+    '3.2.2 (Puppet Enterprise 3.1.3)':  { $uninstallver='PE/3.0.0' }
+    '3.2.2 (Puppet Enterprise 3.0.0)':  { $uninstallver='PE/3.0.0' }
+    '3.2.2 (Puppet Enterprise 3.0.0)':  { $uninstallver='PE/3.0.0' }
     '3.7.0 (Puppet Enterprise 3.7.0)':  { $uninstallver='PE/3.7.0' }
     '3.7.2 (Puppet Enterprise 3.7.1)':  { $uninstallver='PE/3.7.1' }
     '3.7.2 (Puppet Enterprise 3.7.2)':  { $uninstallver='PE/3.7.2' }
