e63e4e874efb7cb7493a28e1463a709a064e4d8a
Chris Matteson <chris.matteson@puppetlabs.com>
2015-09-06 20:19:54 +0000

    added poss support for 3.0.0 and greater

--- manifests/init.pp
+++ manifests/init.pp
@@ -43,37 +43,96 @@ class cutover (
     '3.8.0 (Puppet Enterprise 3.8.0)':  { $uninstallver='PE/3.8.0' }
     '3.8.1 (Puppet Enterprise 3.8.1)':  { $uninstallver='PE/3.8.1' }
     '3.8.2 (Puppet Enterprise 3.8.2)':  { $uninstallver='PE/3.8.2' }
+    '2.6.9':                            { $uninstallver='poss' }
+    '2.6.10':                           { $uninstallver='poss' }
+    '2.6.11':                           { $uninstallver='poss' }
+    '2.6.12':                           { $uninstallver='poss' }
+    '2.6.14':                           { $uninstallver='poss' }
+    '2.6.15':                           { $uninstallver='poss' }
+    '2.6.16':                           { $uninstallver='poss' }
+    '2.6.17':                           { $uninstallver='poss' }
+    '2.6.18':                           { $uninstallver='poss' }
+    '2.7.1':                            { $uninstallver='poss' }
+    '2.7.3':                            { $uninstallver='poss' }
+    '2.7.4':                            { $uninstallver='poss' }
+    '2.7.5':                            { $uninstallver='poss' }
+    '2.7.6':                            { $uninstallver='poss' }
+    '2.7.8':                            { $uninstallver='poss' }
+    '2.7.9':                            { $uninstallver='poss' }
+    '2.7.10':                           { $uninstallver='poss' }
+    '2.7.11':                           { $uninstallver='poss' }
+    '2.7.12':                           { $uninstallver='poss' }
+    '2.7.13':                           { $uninstallver='poss' }
+    '2.7.14':                           { $uninstallver='poss' }
+    '2.7.16':                           { $uninstallver='poss' }
+    '2.7.17':                           { $uninstallver='poss' }
+    '2.7.18':                           { $uninstallver='poss' }
+    '2.7.19':                           { $uninstallver='poss' }
+    '2.7.20':                           { $uninstallver='poss' }
+    '2.7.21':                           { $uninstallver='poss' }
+    '2.7.22':                           { $uninstallver='poss' }
+    '2.7.23':                           { $uninstallver='poss' }
+    '2.7.24':                           { $uninstallver='poss' }
+    '2.7.25':                           { $uninstallver='poss' }
+    '2.7.26':                           { $uninstallver='poss' }
+    '3.0.0':                            { $uninstallver='poss' }
+    '3.0.1':                            { $uninstallver='poss' }
+    '3.0.2':                            { $uninstallver='poss' }
+    '3.1.0':                            { $uninstallver='poss' }
+    '3.1.1':                            { $uninstallver='poss' }
+    '3.2.1':                            { $uninstallver='poss' }
+    '3.2.2':                            { $uninstallver='poss' }
+    '3.2.3':                            { $uninstallver='poss' }
+    '3.2.4':                            { $uninstallver='poss' }
+    '3.3.0':                            { $uninstallver='poss' }
+    '3.3.1':                            { $uninstallver='poss' }
+    '3.3.2':                            { $uninstallver='poss' }
+    '3.4.0':                            { $uninstallver='poss' }
+    '3.4.1':                            { $uninstallver='poss' }
+    '3.4.2':                            { $uninstallver='poss' }
+    '3.4.3':                            { $uninstallver='poss' }
+    '3.5.1':                            { $uninstallver='poss' }
+    '3.6.0':                            { $uninstallver='poss' }
+    '3.6.1':                            { $uninstallver='poss' }
+    '3.6.2':                            { $uninstallver='poss' }
+    '3.7.0':                            { $uninstallver='poss' }
+    '3.7.1':                            { $uninstallver='poss' }
+    '3.7.2':                            { $uninstallver='poss' }
+    '3.7.3':                            { $uninstallver='poss' }
+    '3.7.4':                            { $uninstallver='poss' }
+    '3.7.5':                            { $uninstallver='poss' }
+    '3.8.1':                            { $uninstallver='poss' }
     '3.8.2':                            { $uninstallver='poss' }
     default:                            { fail("Version ${::puppetversion} is unsupported by this module") }
   }
 
   exec { 'create installer':
     command => "/bin/echo 'curl -k https://${new_master}:8140/packages/current/install.bash | sudo bash' | at now + 2 minutes",
-    require => File["/tmp/chrismatteson-cutover/${uninstallver}"],
     cwd     => '/',
-  }
-
-  file { ['/tmp/chrismatteson-cutover','/tmp/chrismatteson-cutover/PE','/tmp/chrismatteson-cutover/POSS']:
-    ensure => directory,
-  }
-
-  file { "/tmp/chrismatteson-cutover/${uninstallver}":
-    ensure  => directory,
-    recurse => true,
-    source  => "puppet:///modules/cutover/${uninstallver}",
-    require => File['/tmp/chrismatteson-cutover/PE'],
   }
 
   if $uninstallver == 'poss' {
     package { ['facter','hiera']:
       ensure => 'absent',
-      before => Package['puppet'],
+      require => Package['puppet'],
     }
     package { ['puppet']:
       ensure  => 'absent',
     }
   }
   else {
+
+    file { ['/tmp/chrismatteson-cutover','/tmp/chrismatteson-cutover/PE']:
+      ensure => directory,
+    }
+
+    file { "/tmp/chrismatteson-cutover/${uninstallver}":
+      ensure  => directory,
+      recurse => true,
+      source  => "puppet:///modules/cutover/${uninstallver}",
+      require => File['/tmp/chrismatteson-cutover/PE'],
+    }
+
     exec { 'cutover':
       command => '/tmp/chrismatteson-cutover/<%= @uninstallver %>/puppet-enterprise-uninstaller -p -a /tmp/chrismatteson-cutover/<%= @uninstallver %>/answers.txt',
       require => Exec['create installer'],
